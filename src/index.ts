78755bef9996f05a3f9996c472a7370741304512
Vitali <gimvit@gmail.com>
2024-10-17 14:25:59 +0200

    feat: implement simple http server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const greeting: string = "Hello, world!";
-console.log(greeting);
+import { config } from "dotenv";
+import { createServer } from "http";
+config();
+
+async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const app = createServer((req, res) => {
+    res.write("ok");
+    res.end();
+  });
+
+  await app.listen(port);
+  console.log(`Server is started at port ${port}`);
+}
+
+bootstrap();
